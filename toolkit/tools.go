33b0e1ab6015b6a707111f3565a4031b52122589
Diego Herrera <daherre0@gmail.com>
2025-10-02 11:17:54 -0500

    feat: add file download functionality and corresponding tests; update README and workspace configuration

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -200,4 +201,15 @@ func (t *Tools) Slugify(s string) (string, error) {
 
 	return slug, nil
 
+}
+
+// DownloadStaticFile downloads a file, and tries to force the browser to avoid displaying it
+// in the browser windows by setting content disposition. It also allows specification of the
+// display name
+func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, p, file, displayName string) {
+	fp := path.Join(p, file)
+	//Download the file instead of displaying it on the browser
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
+
+	http.ServeFile(w, r, fp)
 }
