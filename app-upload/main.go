c6481675401aec9ff2e7b4859bef68e12e5bc83c
Diego Herrera <daherre0@gmail.com>
2025-10-01 23:29:06 -0500

    feat: implement multiple/single file upload  functionality with HTML form and server handling

---
+++ app-upload/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Go-Golang-Training/toolkit-project/toolkit"
+)
+
+func main() {
+	mux := routes()
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func routes() http.Handler {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/upload", uploadFiles)
+	mux.HandleFunc("/upload-one", uploadOneFile)
+
+	return mux
+}
+
+func uploadFiles(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	t := toolkit.Tools{
+		MaxFileSize:      1024 * 1024 * 1024, // 1 GB
+		AllowedFileTypes: []string{"image/jpeg", "image/png", "image/gif"},
+	}
+
+	files, err := t.UploadFiles(r, "./uploads")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	out := ""
+	for _, item := range files {
+		out += fmt.Sprintf("Uploaded %s to the uploads directory, renamed to %s and is %d bytes in size\n", item.OriginalFileName, item.NewFileName, item.FileSize)
+
+	}
+
+	_, _ = w.Write([]byte(out))
+}
+
+func uploadOneFile(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	t := toolkit.Tools{
+		MaxFileSize:      1024 * 1024 * 1024, // 1 GB
+		AllowedFileTypes: []string{"image/jpeg", "image/png", "image/gif"},
+	}
+
+	file, err := t.UploadOneFile(r, "./uploads")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	out := ""
+
+	out += fmt.Sprintf("Uploaded %s to the uploads directory, renamed to %s and is %d bytes in size\n", file.OriginalFileName, file.NewFileName, file.FileSize)
+
+	_, _ = w.Write([]byte(out))
+
+}
--- toolkit/tools.go
+++ toolkit/tools.go
@@ -2,6 +2,13 @@ package toolkit
 
 import (
 	"crypto/rand"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
@@ -9,6 +16,8 @@ const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 // Tools is the type to instantiate this module. Any variable of this type will have access
 // to the methods with the receiver *Tools
 type Tools struct {
+	MaxFileSize      int
+	AllowedFileTypes []string
 	// Add fields if necessary
 }
 
@@ -23,4 +32,135 @@ func (t *Tools) RandomString(n int) string {
 	}
 
 	return string(s)
+}
+
+// UploadedFile is a struct to hold information about an uploaded file
+type UploadedFile struct {
+	OriginalFileName string
+	NewFileName      string
+	FileSize         int64
+}
+
+// UploadOneFile handles a single file upload. It takes an http.Request, the upload directory, and an optional
+// boolean to indicate whether to rename the file. It returns a pointer to an UploadedFile and an error.
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+
+	renameFile := true
+
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		return nil, errors.New("no files were uploaded")
+	}
+
+	return files[0], nil
+
+}
+
+// UploadFile handles file uploads. It takes an http.Request, the upload directory, and an optional
+// boolean to indicate whether to rename the file. It returns a slice of UploadedFile pointers and an error.
+func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
+
+	renameFile := true
+
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	var uploadedFiles []*UploadedFile
+
+	if t.MaxFileSize == 0 {
+		t.MaxFileSize = 1024 * 1024 * 1024 // 1 GB
+	}
+
+	err := r.ParseMultipartForm(int64(t.MaxFileSize))
+
+	if err != nil {
+		return nil, errors.New("the uploaded file is too big. please choose a smaller file")
+	}
+
+	for _, fHeaders := range r.MultipartForm.File {
+		for _, hdr := range fHeaders {
+			uploadedFiles, err = func(uploadedFiles []*UploadedFile) ([]*UploadedFile, error) {
+				var uploadedFile UploadedFile
+				infile, err := hdr.Open()
+				if err != nil {
+					return nil, err
+				}
+				defer infile.Close()
+
+				buff := make([]byte, 512)
+				_, err = infile.Read(buff)
+
+				if err != nil {
+					return nil, err
+				}
+
+				// Check to see if the file type is permitted
+
+				allowed := false
+				fileType := http.DetectContentType(buff)
+
+				if len(t.AllowedFileTypes) > 0 {
+					for _, x := range t.AllowedFileTypes {
+						if strings.EqualFold(x, fileType) {
+							allowed = true
+							break
+						}
+					}
+				} else {
+					allowed = true
+				}
+
+				if !allowed {
+					return nil, errors.New("The file type is not permitted")
+				}
+
+				_, err = infile.Seek(0, 0)
+				if err != nil {
+					return nil, err
+				}
+
+				uploadedFile.OriginalFileName = hdr.Filename
+
+				if renameFile {
+					uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(hdr.Filename))
+				} else {
+					uploadedFile.NewFileName = hdr.Filename
+				}
+
+				var outfile *os.File
+				defer outfile.Close()
+
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+					return nil, err
+				} else {
+					fileSize, err := io.Copy(outfile, infile)
+					if err != nil {
+						return nil, err
+					}
+					uploadedFile.FileSize = fileSize
+				}
+
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+
+				return uploadedFiles, nil
+
+			}(uploadedFiles)
+
+			if err != nil {
+				return uploadedFiles, err
+			}
+
+		}
+	}
+	return uploadedFiles, nil
 }
